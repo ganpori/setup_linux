e7bfb339edec8ba7fc1c809ea9b85a1deea93330
TaikiIwahori <hmyrwjj@yahoo.co.jp>
2023-09-12 15:00:31 +0900

    保存するディレクトリを作成

--- roles/maru_camera/files/take_photo.sh
+++ roles/maru_camera/files/take_photo.sh
@@ -2,5 +2,8 @@
 # --datetimeをつけるとシステムの時刻を使ってMMDDhhmmss.jpgのファイルを作る
 datetime=`date '+%Y%m%d%H%M%S'`  # ファイル名に使う撮影時刻文字列
 degree_rotation=180  # カメラが反転してるから。0か180しか指定できないらしい
+savedirname='camera_images'
 
- /usr/bin/libcamera-still -o $datetime.jpg  --rotation $degree_rotation
+mkdir $savedirname # 保存先を作成
+
+ /usr/bin/libcamera-still -o $savedirname/${datetime}_auto.jpg  --rotation $degree_rotation
