a9deee61692b0368e4f77049261123d67ed37396
TaikiIwahori <hmyrwjj@yahoo.co.jp>
2023-09-12 14:51:23 +0900

    時刻をつけれるようにした

--- roles/maru_camera/files/take_photo.sh
+++ roles/maru_camera/files/take_photo.sh
@@ -1,3 +1,5 @@
 # --rotation 180 はその時のカメラの設置位置しだい。０か180しかだめらしい。
 # --datetimeをつけるとシステムの時刻を使ってMMDDhhmmss.jpgのファイルを作る
- /usr/bin/libcamera-still --datetime  --rotation 180
+datetime=`date '+%Y%m%d%H%M%S'`  # ファイル名に使う撮影時刻文字列
+
+ /usr/bin/libcamera-still -o $datetime.jpg  --rotation 180
