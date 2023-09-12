ffbf91373b5a7fabc37b1b6be218bad1fa8b9d2f
TaikiIwahori <hmyrwjj@yahoo.co.jp>
2023-09-12 17:59:20 +0900

    真っ暗でも撮影できるように露光時間とゲインを増やした

--- roles/maru_camera/files/take_photo.sh
+++ roles/maru_camera/files/take_photo.sh
@@ -3,8 +3,9 @@
 datetime=`date '+%Y%m%d%H%M%S'`  # ファイル名に使う撮影時刻文字列
 degree_rotation=180  # カメラが反転してるから。0か180しか指定できないらしい
 savedirname='camera_images'
-exposure_seconds=10  # 最大露光時間は112秒, https://www.raspberrypi.com/documentation/accessories/camera.html#hardware-specification
+exposure_seconds=30  # 最大露光時間は112秒, https://www.raspberrypi.com/documentation/accessories/camera.html#hardware-specification
 exposure_microseconds=$((exposure_seconds * 10**6))  # libcameraでの露光時間の単位はμ秒なので単位変換。
+gain_at_long_exposure=128.0  # ふつうは１.0付近の値なんだけど暗所でやるためにデカ盛りの値。
 
 mkdir $savedirname # 保存先を作成
 
@@ -12,4 +13,4 @@ mkdir $savedirname # 保存先を作成
 # https://www.raspberrypi.com/documentation/computers/camera_software.html
 /usr/bin/libcamera-still -o $savedirname/${datetime}_auto.jpg  --rotation $degree_rotation --immediate
 # 非常に長時間露光の画像をキャプチャするには、AEC/AGC および AWB を無効にするように注意する必要があります。無効にしないと、これらのアルゴリズムにより、ユーザーは収束するまでに多数のフレームを待機することになります。
-/usr/bin/libcamera-still -o $savedirname/${datetime}_long_exposure.jpg   --rotation $degree_rotation --shutter $exposure_microseconds --immediate  --gain 1 --awbgains 1,1
+/usr/bin/libcamera-still -o $savedirname/${datetime}_long_exposure.jpg   --rotation $degree_rotation --shutter $exposure_microseconds --immediate  --gain $gain_at_long_exposure --awbgains 1,1
