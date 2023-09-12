0481fa912e466cecc1a687982797be1e7e1d2c21
TaikiIwahori <hmyrwjj@yahoo.co.jp>
2023-09-12 14:52:33 +0900

    回転量を変数化

--- roles/maru_camera/files/take_photo.sh
+++ roles/maru_camera/files/take_photo.sh
@@ -1,5 +1,6 @@
 # --rotation 180 はその時のカメラの設置位置しだい。０か180しかだめらしい。
 # --datetimeをつけるとシステムの時刻を使ってMMDDhhmmss.jpgのファイルを作る
 datetime=`date '+%Y%m%d%H%M%S'`  # ファイル名に使う撮影時刻文字列
+degree_rotation=180  # カメラが反転してるから。0か180しか指定できないらしい
 
- /usr/bin/libcamera-still -o $datetime.jpg  --rotation 180
+ /usr/bin/libcamera-still -o $datetime.jpg  --rotation $degree_rotation
